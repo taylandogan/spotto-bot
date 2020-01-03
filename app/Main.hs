38c766db6824160f5f4e41fe98268e9d659e220b
Taylan Dogan <ttaylan.dogann@gmail.com>
2020-01-03 21:10:16 +0100

    Simple GET request with wreq

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Lib
 
+import Control.Lens
+import Data.Aeson (toJSON)
+import Data.Aeson.Lens (key, nth)
+import qualified Network.Wreq as Wreq
+import qualified Network.Wreq.Session as WSess
+
 main :: IO ()
-main = someFunc
+main = do
+    sess <- WSess.newAPISession
+    response <- WSess.get sess "http://google.com"
+    print response
