99446826be4ec86f535f484e4163c98719bbc334
Taylan Dogan <ttaylan.dogann@gmail.com>
2020-01-14 16:51:38 +0100

    Added newtype Token

--- app/Main.hs
+++ app/Main.hs
@@ -8,12 +8,14 @@ import Control.Lens
 import Data.Aeson (toJSON)
 import Data.Aeson.Lens (key, _String)
 import Data.ByteString.Base64 (encode)
-import Data.Text (unpack)
+import qualified Data.Text as T
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
 import Network.Wreq (defaults, header, responseBody, FormParam((:=)))
 import Network.Wreq.Session (Session, postWith, newAPISession)
 import System.Environment (getEnv)
+
+newtype Token = MkToken T.Text deriving(Show)
 
 baseSpotifyUrl :: String
 baseSpotifyUrl = "https://api.spotify.com"
@@ -28,15 +30,15 @@ getAuthHeader = do
     let concatted = B8.pack (appId ++ ":" ++ appSecret)
     return $ "Basic " ++ (B8.unpack . encode $ concatted)
 
-getToken :: Session -> IO (Maybe String)
+getToken :: Session -> IO (Maybe Token)
 getToken sess = do
     authHeader <- getAuthHeader
     let opts = defaults & header "Authorization" .~ [B8.pack authHeader]
     resp <- postWith opts sess tokenUrl ["grant_type" := ("client_credentials" :: String)]
-    return $ unpack <$> resp ^? responseBody . key "access_token" . _String
+    return $ MkToken <$> resp ^? responseBody . key "access_token" . _String
 
 main :: IO ()
 main = do
     sess <- newAPISession
-    mbAccessToken <- getToken sess
-    print mbAccessToken
+    mbToken <- getToken sess
+    print mbToken
