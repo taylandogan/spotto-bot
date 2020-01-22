328f20cd559f1e456abe1a75ad7a4b67585add34
Taylan Dogan <ttaylan.dogann@gmail.com>
2020-01-22 19:11:37 +0100

    Added some exception handling

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,22 @@ module Main where
 
 import Lib
 
+import Control.Exception as E
 import Control.Lens
+import Control.Lens.Combinators (_Right)
 import Data.Aeson (toJSON)
 import Data.Aeson.Lens (key, _String)
 import Data.ByteString.Base64 (encode)
+import Data.Either.Utils (maybeToEither, fromRight)
 import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B8
-import Network.Wreq (defaults, header, responseBody, FormParam((:=)))
-import Network.Wreq.Session (Session, postWith, newAPISession)
+import qualified Data.ByteString.Lazy.Internal as LBS
+import Data.Maybe (fromMaybe)
+import Network.HTTP.Client as CL
+import Network.Wreq as W
+import Network.Wreq.Session as WS
 import System.Environment (getEnv)
 
 newtype Token = MkToken T.Text deriving(Show)
@@ -30,15 +37,19 @@ getAuthHeader = do
     let concatted = B8.pack (appId ++ ":" ++ appSecret)
     return $ "Basic " ++ (B8.unpack . encode $ concatted)
 
-getToken :: Session -> IO (Maybe Token)
+requestHandler :: HttpException -> IO (Either String (Response LBS.ByteString))
+requestHandler (HttpExceptionRequest _ exceptionContent) = return $ Left $ show exceptionContent
+requestHandler (InvalidUrlException url reason) = return $ Left $ "Invalid url: " ++ url ++ " Reason: " ++ reason
+
+getToken :: WS.Session -> IO (Either String Token)
 getToken sess = do
     authHeader <- getAuthHeader
-    let opts = defaults & header "Authorization" .~ [B8.pack authHeader]
-    resp <- postWith opts sess tokenUrl ["grant_type" := ("client_credentials" :: String)]
-    return $ MkToken <$> resp ^? responseBody . key "access_token" . _String
+    let opts = W.defaults & W.header "Authorization" .~ [B8.pack authHeader]
+    resp <- (Right <$> WS.postWith opts sess tokenUrl ["grant_type" := ("client_credentials" :: String)]) `E.catch` requestHandler
+    return $ MkToken <$> maybeToEither "Could not retrieve token" (resp ^? _Right . W.responseBody . key "access_token" . _String)
 
 main :: IO ()
 main = do
     sess <- newAPISession
-    mbToken <- getToken sess
-    print mbToken
+    eiToken <- getToken sess
+    print eiToken
