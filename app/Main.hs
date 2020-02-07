3fe0b2246960e3ce76dffb3a850057257306dd04
Taylan Dogan <ttaylan.dogann@gmail.com>
2020-02-07 16:40:38 +0100

    Updating request options in a cleaner way

--- app/Main.hs
+++ app/Main.hs
@@ -18,11 +18,13 @@ import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy.Internal as LBS
 import Data.Maybe (fromMaybe)
 import Network.HTTP.Client as CL
+import Network.HTTP.Types.Header as HH
 import Network.Wreq as W
 import Network.Wreq.Session as WS
+import Network.Wreq.Types as WT
 import System.Environment (getEnv)
 
-newtype Token = MkToken T.Text deriving(Show)
+newtype Token = MkToken T.Text deriving (Show)
 
 baseSpotifyUrl :: String
 baseSpotifyUrl = "https://api.spotify.com"
@@ -30,12 +32,12 @@ baseSpotifyUrl = "https://api.spotify.com"
 tokenUrl :: String
 tokenUrl = "https://accounts.spotify.com/api/token"
 
-getAuthHeader :: IO String
+getAuthHeader :: IO T.Text
 getAuthHeader = do
     appId <- getEnv "SPOTIFY_APP_ID"
     appSecret <- getEnv "SPOTIFY_APP_SECRET"
     let concatted = B8.pack (appId ++ ":" ++ appSecret)
-    return $ "Basic " ++ (B8.unpack . encode $ concatted)
+    return . T.pack $ "Basic " ++ (B8.unpack . encode $ concatted)
 
 requestHandler :: HttpException -> IO (Either String (Response LBS.ByteString))
 requestHandler (HttpExceptionRequest _ exceptionContent) = return $ Left $ show exceptionContent
@@ -44,9 +46,18 @@ requestHandler (InvalidUrlException url reason) = return $ Left $ "Invalid url: 
 getToken :: WS.Session -> IO (Either String Token)
 getToken sess = do
     authHeader <- getAuthHeader
-    let opts = W.defaults & W.header "Authorization" .~ [B8.pack authHeader]
+    let opts = addHeader "Authorization" authHeader W.defaults
     resp <- (Right <$> WS.postWith opts sess tokenUrl ["grant_type" := ("client_credentials" :: String)]) `E.catch` requestHandler
     return $ MkToken <$> maybeToEither "Could not retrieve token" (resp ^? _Right . W.responseBody . key "access_token" . _String)
+
+getBearerStr :: Token -> T.Text
+getBearerStr (MkToken token) = T.pack $ "Bearer " ++ T.unpack token
+
+addHeader :: HH.HeaderName -> T.Text -> WT.Options -> WT.Options
+addHeader name value opts = opts & W.header name .~ [encodeUtf8 value]
+
+addQueryParam :: T.Text -> T.Text -> WT.Options -> WT.Options
+addQueryParam name value opts = opts & W.param name .~ [value]
 
 main :: IO ()
 main = do
