c38c42eaef31ecc91099d1ae18891af69f0c7ac2
Taylan Dogan <ttaylan.dogann@gmail.com>
2020-03-05 01:21:51 +0100

    Attempt to parse response to PlaylistItem

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,12 @@ import Utils
 import Control.Lens
 import Control.Monad.Except
 import Control.Monad.Trans.Except
-import Data.Aeson (toJSON, FromJSON, ToJSON, decode)
+import Data.Aeson (toJSON, FromJSON, ToJSON, decode, eitherDecode)
 import Data.Aeson.Lens (key, _String, _Array)
-import Data.Aeson.Types (Value)
-import Data.Either.Utils (maybeToEither, fromRight)
-import Data.Vector (Vector)
+import Data.Aeson.Types (Value, parseJSON, parseEither, parseMaybe)
+import Data.Either.Utils (maybeToEither)
+import Data.Vector ((!?))
+import qualified Data.ByteString.Lazy as B
 import qualified Data.Text as T
 import qualified Network.Wreq as W
 import qualified Network.Wreq.Session as WS
@@ -26,17 +27,22 @@ getToken sess = do
     -- resp <- raiseResponse "Request to get token" resp
     return $ MkToken (resp ^. W.responseBody . key "access_token" . _String)
 
-getMyPlaylists :: WS.Session -> Token -> HTTPIO (Vector Value)
+parsePlaylist :: Maybe Value -> HTTPIO PlaylistItem
+parsePlaylist Nothing = throwError "Cannot parse empty JSON to playlist"
+parsePlaylist (Just val) = case parseEither parseJSON val of
+                            Left err -> throwError err
+                            Right pl -> return pl
+
+getMyPlaylists :: WS.Session -> Token -> HTTPIO PlaylistItem
 getMyPlaylists sess token = do
     let playlistUrl = baseSpotifyUrl ++ "/v1/users/taylandgn/playlists"
     let opts = addQueryParam "offset" "0" . addQueryParam "limit" "1" . addHeader "Authorization" (getBearerStr token) $ W.defaults
     resp <- liftIO (WS.getWith opts sess playlistUrl) `catchE` requestHandler
-    return $ resp ^. W.responseBody . key "items" . _Array
+    parsePlaylist $ (resp ^. W.responseBody . key "items" . _Array) !? 0
+    -- TODO: Adapt the code to work on the whole array at once (mapM parsePlaylist ..)
 
 main :: IO ()
 main = do
     sess <- WS.newAPISession
-    eiToken <- runExceptT $ getToken sess
-    case eiToken of
-        Left err -> print err
-        Right token -> print =<< (runExceptT $ getMyPlaylists sess token)
+    eiPlaylists <- runExceptT $ getMyPlaylists sess =<< getToken sess
+    print eiPlaylists
