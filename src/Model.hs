3860f92b56991760597ba381da9adc368004f1e9
Taylan Dogan <ttaylan.dogann@gmail.com>
2020-03-07 00:14:52 +0100

    Get rid of unnecessary data types

--- src/Model.hs
+++ src/Model.hs
@@ -2,8 +2,6 @@
 
 module Model
     ( Token (..),
-      Track,
-      Tracks,
       PlaylistItem,
       HTTPIO,
       InfoMsg
@@ -18,23 +16,12 @@ type InfoMsg = String
 type HTTPIO = ExceptT String IO
 newtype Token = MkToken Text deriving (Show)
 
-data Track = Track { track_name :: ! Text } deriving (Show, Generic)
-instance FromJSON Track
-instance ToJSON Track
-
-data Tracks = Tracks { tracks_href :: !Text
-                    , total :: Int
-                    } deriving (Show, Generic)
-instance FromJSON Tracks
-instance ToJSON Tracks
-
 data PlaylistItem =
     PlaylistItem { description :: !Text
                   , href :: !Text
                   , id :: !Text
                   , name :: !Text
                   , snapshot_id :: !Text
-                  , tracks :: Tracks
-                    } deriving (Show, Generic)
+                   } deriving (Show, Generic)
 instance FromJSON PlaylistItem
 instance ToJSON PlaylistItem
