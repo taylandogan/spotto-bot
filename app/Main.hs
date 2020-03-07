aaef28157c9246302d3429f8273497fa25b8c2d3
Taylan Dogan <ttaylan.dogann@gmail.com>
2020-03-07 13:05:08 +0100

    Collect all the playlists in a vector

--- app/Main.hs
+++ app/Main.hs
@@ -12,12 +12,15 @@ import Data.Aeson (toJSON, FromJSON, ToJSON, decode, eitherDecode)
 import Data.Aeson.Lens (key, _String, _Array)
 import Data.Aeson.Types (Value, parseJSON, parseEither, parseMaybe)
 import Data.Either.Utils (maybeToEither)
-import Data.Vector (Vector, (!?))
+import Data.Vector (Vector, concat, (!?))
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Text as T
 import qualified Network.Wreq as W
 import qualified Network.Wreq.Session as WS
 
+
+username :: String
+username = "taylandgn"
 
 getToken :: WS.Session -> HTTPIO Token
 getToken sess = do
@@ -30,17 +33,26 @@ getToken sess = do
 parsePlaylist :: Value -> HTTPIO PlaylistItem
 parsePlaylist val = case parseEither parseJSON val of
                             Left err -> throwError err
-                            Right pl -> return pl
+                            Right playlist -> return playlist
 
-getMyPlaylists :: WS.Session -> Token -> HTTPIO (Vector PlaylistItem)
-getMyPlaylists sess token = do
-    let playlistUrl = baseSpotifyUrl ++ "/v1/users/taylandgn/playlists"
-    let opts = addQueryParam "offset" "0" . addQueryParam "limit" "32" . addHeader "Authorization" (getBearerStr token) $ W.defaults
+getPlaylists :: Int -> Int -> String -> WS.Session -> Token -> HTTPIO (Vector PlaylistItem)
+getPlaylists offset limit username sess token = do
+    let offsetTxt = T.pack . show $ offset
+    let limitTxt = T.pack . show $ limit
+    let playlistUrl = baseSpotifyUrl ++ "/v1/users/" ++ username  ++ "/playlists"
+    let opts = addQueryParam "offset" offsetTxt . addQueryParam "limit" limitTxt . addHeader "Authorization" (getBearerStr token) $ W.defaults
     resp <- liftIO (WS.getWith opts sess playlistUrl) `catchE` requestHandler
     mapM parsePlaylist (resp ^. W.responseBody . key "items" . _Array)
+
+-- TODO: Do this until you get a empty list (but like who has 250 playlist?)
+collectPlaylists :: String -> WS.Session -> Token -> HTTPIO (Vector PlaylistItem)
+collectPlaylists username sess token = Data.Vector.concat <$> traverse (\offset-> getPlaylists offset 50 username sess token) [0..5]
 
 main :: IO ()
 main = do
     sess <- WS.newAPISession
-    eiPlaylists <- runExceptT $ getMyPlaylists sess =<< getToken sess
+    eiPlaylists <- runExceptT $ collectPlaylists username sess =<< getToken sess
     print eiPlaylists
+    -- case eiPlaylists of
+    --     Left err -> print err
+    --     Right ps -> ps
